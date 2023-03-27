2033d77c3fd6a2b8b616c760a6fb902bdb6678c7
uglyboy12 <fariddarel3@gmail.com>
2023-03-27 10:25:21 +0700

    Initial commit

---
+++ administrator/cache/_media_version/131a5aeebf41db56945fff019fb72b51-cache-_media_version-2045f49da091907ae6a263f569e53b90.php
@@ -0,0 +1,1 @@
+<?php die("Access Denied"); ?>#x#a:2:{s:6:"result";s:32:"176ca401de3bec56e8d49c67072c6038";s:6:"output";s:0:"";}
---
+++ administrator/logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2023-03-27 01:58:01 UTC
+#Software: Joomla! 4.2.9 Stable [ Uaminifu ] 14-March-2023 15:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2023-03-27T01:58:01+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2023-03-27T01:59:09+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
